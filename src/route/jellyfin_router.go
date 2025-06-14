9ea7f8919c008a7ae649a3154733b01ca239e26e
H3llmy <helmytrisna86@gmail.com>
2025-06-14 16:41:04 +0800

    feat: implement dependency injection for router setup and update routes for Jellyfin and monitoring

--- src/route/jellyfin_router.go
+++ src/route/jellyfin_router.go
@@ -3,13 +3,16 @@ package router
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/h3llmy/system-monitoring/src/controller"
+	"go.uber.org/dig"
 )
 
 // JellyfinRouter adds routes for accessing jellyfin metrics.
 //
 // The /jellyfin route is added to the provided version group.
-func JellyfinRouter(version fiber.Router, controller *controller.JellyfinController){
+func JellyfinRouter(version fiber.Router, container *dig.Container) {
 	jellyfinRoute := version.Group("/jellyfin")
 
-	jellyfinRoute.Get("/Count", controller.GetJellyfinCount)
+	container.Invoke(func(controller *controller.JellyfinController) {
+		jellyfinRoute.Get("/Count", controller.GetJellyfinCount)
+	})
 }
--- src/route/router.go
+++ src/route/router.go
@@ -8,23 +8,35 @@ import (
 	httpClient "github.com/h3llmy/system-monitoring/src/utils/httpClient"
 
 	"github.com/gofiber/fiber/v2"
+	"go.uber.org/dig"
 )
 
-// Routes sets up all routes for the application.
+// Routes sets up all routes for the application using dependency injection.
 func Routes(app *fiber.App) {
-	// utils initialize
-	httpClient := httpClient.NewClient(10 * time.Second)
-
-	// services initialize
-	monitoringService := service.NewSystemMonitorService()
-	jfService := service.NewJellyfinService(httpClient)
-
-	// controllers initialize
-	monitoringController := controller.NewMonitoringController(monitoringService)
-	jellyfinController := controller.NewJellyfinController(*jfService)
+	container := setupContainer()
 
 	v1 := app.Group("/api/v1")
 
-	MonitoringRoutes(v1, monitoringController)
-	JellyfinRouter(v1, jellyfinController)
+	MonitoringRoutes(v1, container)
+	JellyfinRouter(v1, container)
+}
+
+// SetupContainer initializes the DI container and registers all dependencies.
+func setupContainer() *dig.Container {
+	container := dig.New()
+
+	// Provide utility
+	container.Provide(func() *httpClient.Client {
+		return httpClient.NewClient(10 * time.Second)
+	})
+
+	// Provide services
+	container.Provide(service.NewSystemMonitorService)
+	container.Provide(service.NewJellyfinService)
+
+	// Provide controllers
+	container.Provide(controller.NewMonitoringController)
+	container.Provide(controller.NewJellyfinController)
+
+	return container
 }
